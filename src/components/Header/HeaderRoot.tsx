cd721b883debfa81e30de81b360b7a4901baf78e
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-11-17 21:59:19 -0400

    :lipstick: chore:  Fixed Top

--- src/components/Header/HeaderRoot.tsx
+++ src/components/Header/HeaderRoot.tsx
@@ -6,7 +6,7 @@ type HeaderRootProps = {
 
 export default function HeaderRoot({ children }: HeaderRootProps) {
   return (
-    <header className="w-full h-20 flex justify-between  items-center px-8 md:justify-between md:px-6">
+    <header className=" fixed z-10 bg-white w-full h-14 md:h-20 flex justify-between  items-center px-8 md:justify-between md:px-6">
       {children}
     </header>
   );
